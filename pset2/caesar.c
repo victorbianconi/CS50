0f6f6affa90e68cf524e3bebcf95b35fc023d9f7
Victor <64693573+victorbianconi@users.noreply.github.com>
2020-12-10 18:42:21 +0100

    Create caesar.c

---
+++ pset2/caesar.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+  if (argc == 2)
+   {
+     for(int i = 0; argv[1][i] != 0; i++)
+     {
+         if( argv[1][i] < 48 || argv[1][i] > 57)
+         {
+                  printf("Usage: ./caesar key\n");
+                  return 1;
+         }
+     }
+     
+      int key = atoi(argv[1]);
+
+      string p = get_string("plaintext: ");
+      string upper = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+      string lower = {"abcdefghijklmnopqrstuvwxyz"};
+
+      // string c[count];
+      //printf("%i", count);
+      //char c[count];
+      //  printf("%c", c);
+      printf("ciphertext: ");
+
+      for(int i = 0, n = strlen(p); i < n; i++)
+      { 
+             if islower(p[i])
+              {
+                   printf("%c", (((p[i] + key) - 97) % 26) + 97);
+              }
+             else if isupper(p[i]) 
+              {
+                   printf("%c", (((p[i] + key) - 65) % 26) + 65);   
+              }
+             else
+              {
+                   printf("%c", p[i]);
+              }
+      }
+      
+      printf("\n");
+      return 0;
+
+   } else 
+   {
+      printf("Usage: ./caesar key\n");
+      return 1;
+   }
+}
